7cd1bc6669ac469727961c3ee67dc0fb32a1e9aa
trox <masalox5@gmail.com>
2024-09-19 14:07:12 +0300

    Update main.cpp

--- Calorie Counter/main.cpp
+++ Calorie Counter/main.cpp
@@ -6,7 +6,6 @@
 #include <unordered_map>
 #include <limits>
 #include <fstream>
-#include "starybox.h"
 using namespace std;
 //чтото менятся
 // Структура для хранения информации о пользователе
@@ -142,6 +141,51 @@ void writePersonToFile(const std::string& filename, User& person) {
         std::cerr << "Ошибка открытия файла!" << std::endl;
     }
 }
+bool findConcatenatedString(const std::string& filename, const std::string& firstWord, const std::string& secondWord,
+    std::string& firstString, int& firstInt, std::string& secondString, int& secondInt) {
+    std::ifstream inFile(filename);
+    if (!inFile.is_open()) {
+        std::cerr << "Не удалось открыть файл " << filename << std::endl;
+        return false;
+    }
+    // Склеиваем слова
+    std::string searchString = firstWord + "_" + secondWord;
+    std::string line;
+    // Ищем строку
+    while (std::getline(inFile, line)) {
+        if (line.find(searchString) != std::string::npos) {
+            std::istringstream iss(line);
+            std::string tempString;
+            // Читаем строку до первого подчеркивания
+            if (std::getline(iss, tempString, '_')) {
+                firstString = tempString; // Сохраняем первую строку
+            }
+            if (std::getline(iss, tempString, '_')) {
+                secondString = tempString; // Сохраняем вторую строку
+            }
+            // Читаем целое число
+            if (!(iss >> firstInt)) {
+                std::cerr << "Ошибка при чтении первого числа." << std::endl;
+                inFile.close();
+                return false;
+            }
+            iss.ignore(); // Игнорируем следующий символ подчеркивания
+            // Читаем вторую строку
+            // Читаем второе целое число
+            if (!(iss >> secondInt)) {
+                std::cerr << "Ошибка при чтении второго числа." << std::endl;
+                inFile.close();
+                return false;
+            }
+                std::cout << "Успешный вход!" << std::endl;
+                inFile.close();
+                return true;
+            }
+        }
+        std::cout << "Строка не найдена." << std::endl;
+        inFile.close();
+        return false;
+    }
 
 void createFile(User& user, int number) {
     // Получаем текущее время и форматируем его
