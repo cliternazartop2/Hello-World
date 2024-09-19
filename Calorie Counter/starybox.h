7317189bf08491548dac854bac70f23e454a1817
trox <masalox5@gmail.com>
2024-09-19 13:40:16 +0300

    Update and rename starybox to starybox.h

---
+++ Calorie Counter/starybox.h
@@ -0,0 +1,4 @@
+
+#include "starybox.h"
+bool findConcatenatedString(const std::string& filename, const std::string& firstWord, const std::string& secondWord,
+    std::string& firstString, int& firstInt, std::string& secondString, int& secondInt) {
