35b6620df5502ed198b5081f37d2d88cb09714a8
trox <masalox5@gmail.com>
2024-09-19 14:01:24 +0300

    Update starybox.h

--- Calorie Counter/starybox.h
+++ Calorie Counter/starybox.h
@@ -1,4 +1,1 @@
 
-#include "starybox.h"
-bool findConcatenatedString(const std::string& filename, const std::string& firstWord, const std::string& secondWord,
-    std::string& firstString, int& firstInt, std::string& secondString, int& secondInt) {
